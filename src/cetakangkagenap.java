aca3591cf98116aefc53c05d8971a169ecb6f27c
HP COREi3 <r42615211@gmail.com>
2023-07-14 17:25:06 +0700

    Initial commit

---
+++ src/cetakangkagenap.java
@@ -0,0 +1,14 @@
+public class cetaknilai_tanpaPERULANGAN {
+    private int [] S = new int[]{1,5,6,7,12,3,24,9,18};
+    public void cetakangkagenap(int index){
+        if (S[index] % 2 == 0) {
+            System.out.println(S[index]);
+        }
+        index ++;
+        cetakangkagenap(index);
+    }
+
+    public void main(String[] args) {
+        cetakangkagenap(0);
+    }
+}
